a1d6787085e18527715f448059f0acd5c1bcc65c
IdoKendo <ryuusuke@gmail.com>
2024-12-20 18:29:06 +0200

    solve: year 2024 day 20

--- cmd/year2024/day20/cmd.go
+++ cmd/year2024/day20/cmd.go
@@ -2,6 +2,8 @@ package day20
 
 import (
 	"fmt"
+	"image"
+	"math"
 	"os"
 	"strings"
 
@@ -24,7 +26,7 @@ func execute(parent, command string) {
 		os.Exit(1)
 	}
 
-	result := Part1(string(content))
+	result := Part1(string(content), 100)
 	fmt.Println("Part 1 result: ", result)
 
 	content, err = os.ReadFile(fmt.Sprintf("cmd/year%s/%s/input2.txt", parent, command))
@@ -33,56 +35,78 @@ func execute(parent, command string) {
 		os.Exit(1)
 	}
 
-	result = Part2(string(content))
+	result = Part2(string(content), 100)
 	fmt.Println("Part 2 result: ", result)
 }
 
-func isPossible(combination string, towels map[string]bool) bool {
-	if towels[combination] || combination == "" {
-		return true
-	}
-	if len(combination) == 1 && !towels[combination] {
-		return false
-	}
+type maze struct {
+	grid  map[image.Point]bool
+	start image.Point
+}
 
-	possible := false
-	for i := range len(combination) {
-		if towels[combination[:i]] {
-			possible = possible || isPossible(combination[i+1:], towels)
-			if possible {
-				return true
+func newMaze(lines []string) maze {
+	grid := make(map[image.Point]bool)
+	var start image.Point
+	for y, line := range lines {
+		for x, p := range line {
+			if p != '#' {
+				point := image.Point{x, y}
+				grid[point] = true
+				if p == 'S' {
+					start = point
+				}
+			}
+		}
+	}
+	return maze{grid, start}
+}
+
+func (m maze) countCheats(save, duration int) int {
+	dist := make(map[image.Point]int)
+	dist[m.start] = 0
+	queue := []image.Point{m.start}
+
+	for len(queue) > 0 {
+		pos := queue[0]
+		queue = queue[1:]
+		for _, p := range []image.Point{
+			{X: pos.X - 1, Y: pos.Y},
+			{X: pos.X + 1, Y: pos.Y},
+			{X: pos.X, Y: pos.Y - 1},
+			{X: pos.X, Y: pos.Y + 1},
+		} {
+			if m.grid[p] && dist[p] == 0 {
+				dist[p] = dist[pos] + 1
+				queue = append(queue, p)
 			}
 		}
 	}
 
-	return false
-}
+	var cheatCount int
 
-func Part1(input string) int {
-	parts := strings.Split(input, "\n\n")
-	towelList := strings.Split(parts[0], ", ")
-	combinations := strings.Split(parts[1], "\n")
-	combinations = combinations[:len(combinations)-1]
-	possibleCombos := 0
-	towels := make(map[string]bool)
-
-	fmt.Println("Towels we have:")
-	for _, t := range towelList {
-		fmt.Println(t)
-		towels[t] = true
-	}
-
-	fmt.Println()
-	for _, combination := range combinations {
-		if isPossible(combination, towels) {
-			fmt.Println("Possible: ", combination)
-			possibleCombos++
+	for point1, dist1 := range dist {
+		for point2, dist2 := range dist {
+			if point1 == point2 {
+				continue
+			}
+			manhattan := int(math.Abs(float64(point1.X-point2.X)) + math.Abs(float64(point1.Y-point2.Y)))
+			if manhattan <= duration && (dist2-dist1-manhattan) >= save {
+				cheatCount++
+			}
 		}
 	}
 
-	return possibleCombos // < 249
+	return cheatCount
 }
 
-func Part2(input string) int {
-	return 0
+func Part1(input string, save int) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	m := newMaze(lines)
+	return m.countCheats(save, 2)
+}
+
+func Part2(input string, save int) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	m := newMaze(lines)
+	return m.countCheats(save, 20) + 1
 }
--- cmd/year2024/day21/cmd_test.go
+++ cmd/year2024/day21/cmd_test.go
@@ -1,38 +1,35 @@
-package day20_test
+package day21_test
 
 import (
 	"os"
 	"testing"
 
-	"github.com/idokendo/aoc/cmd/year2024/day20"
+	"github.com/idokendo/aoc/cmd/year2024/day21"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParts(t *testing.T) {
 	tests := []struct {
 		want  int
-		save  int
 		input string
-		fn    func(string, int) int
+		fn    func(string) int
 	}{
 		{
-			want:  8,
-			save:  12,
+			want:  0,
 			input: "test1.txt",
-			fn:    day20.Part1,
+			fn:    day21.Part1,
 		},
 		{
-			want:  41,
-			save:  70,
+			want:  0,
 			input: "test2.txt",
-			fn:    day20.Part2,
+			fn:    day21.Part2,
 		},
 	}
 
 	for _, tt := range tests {
 		content, err := os.ReadFile(tt.input)
 		assert.NoError(t, err)
-		got := tt.fn(string(content), tt.save)
+		got := tt.fn(string(content))
 		assert.Equal(t, tt.want, got)
 	}
 }
